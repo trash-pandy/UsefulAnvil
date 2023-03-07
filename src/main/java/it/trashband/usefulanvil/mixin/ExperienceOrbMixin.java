853c643d06c3f7926d5a0e0ea1dda9f9f56c2e07
trash-pandy <evancalderon15@gmail.com>
2023-03-07 11:39:41 -0700

    1.0.1: Added efficacy tooltip

--- src/main/java/it/trashband/usefulanvil/mixin/ExperienceOrbMixin.java
+++ src/main/java/it/trashband/usefulanvil/mixin/ExperienceOrbMixin.java
@@ -13,21 +13,23 @@ public class ExperienceOrbMixin {
         var item_damage = item.getDamageValue();
         var max_damage = item.getMaxDamage();
         var repair_amount_was = item_damage - i;
-        var tag = item.getOrCreateTag();
-        if (!tag.contains("usefulanvil.mended_health")) {
-            tag.putInt("usefulanvil.mended_health", 0);
-        }
+        var tag = item.getOrCreateTagElement("it.trashband.usefulanvil");
         // maximum mendable health = 3 complete repairs
         var max_mended_health = max_damage * 3;
         // current amount of health mended
-        var mended_health = tag.getInt("usefulanvil.mended_health");
+        var mended_health = 0;
+        if (tag.contains("mended_health")) {
+            mended_health = tag.getInt("mended_health");
+        } else {
+            tag.putInt("mended_health", 0);
+        }
         // percentage of max mend cap taken
         var mended_percent = Math.min((double) mended_health / max_mended_health, 1.0);
         // reduce the amount that will be repaired to a minimum of 1, as long as the total mended health isn't over the
         // maximum amount of health that can be mended
         var repair_amount = (int) Math.max(Math.round(repair_amount_was * (1.0 - mended_percent)), mended_health >= max_mended_health ? 0 : 1);
         // save total mended health
-        tag.putInt("usefulanvil.mended_health", Math.min(mended_health + repair_amount, max_mended_health));
+        tag.putInt("mended_health", Math.min(mended_health + repair_amount, max_mended_health));
 
         // repair item
         item.setDamageValue(Math.max(item_damage - repair_amount, 0));
